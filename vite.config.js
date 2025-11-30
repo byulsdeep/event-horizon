dd0a46830125c96da5342e7cea847ed5fd4cc6d2
byulsdeep <rakuenthecross@gmail.com>
2025-12-01 00:03:04 +0900

    deploy setup

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,20 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/event-horizon/', 
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'Event Horizon Chat',
+        name: 'Event Horizon',
         short_name: 'Horizon',
         description: 'Private Chat App',
         theme_color: '#ffffff',
         display: 'standalone', // Makes it look like an app on mobile
+        start_url: '/event-horizon',
+        scope: '/event-horizon',
         icons: [
           {
             src: 'pwa-192x192.png', // You can add these icons to /public later
