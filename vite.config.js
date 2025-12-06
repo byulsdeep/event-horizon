c6ba4715fcaf7670bcd66ad0099f81d1194bd168
byulsdeep <rakuenthecross@gmail.com>
2025-12-07 01:09:35 +0900

    Patch 1.0.1: Fix mobile viewport and PWA icons

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,8 @@ export default defineConfig({
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
